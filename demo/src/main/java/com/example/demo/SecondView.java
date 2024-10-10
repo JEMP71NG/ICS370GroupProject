a9cf3c45d4011021dbcf80eed7614b11d04c3855
EMP71NG <nf3963kd@go.minnstate.edu>
2024-10-09 20:57:26 -0500

    deleted the Customer.java class since it was part of the instructor's original package

--- demo/src/main/java/com/example/demo/SecondView.java
+++ demo/src/main/java/com/example/demo/SecondView.java
@@ -1,17 +1,11 @@
 package com.example.demo;
 
-import com.example.demo.domain.Customer;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
