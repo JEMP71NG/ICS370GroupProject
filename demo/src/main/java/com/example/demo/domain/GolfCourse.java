2bf03d244c55f63a2b71f60b0f847b9acd2805d6
EMP71NG <nf3963kd@go.minnstate.edu>
2024-10-09 20:04:19 -0500

    created constructor and get methods for the GolfCourse class

--- demo/src/main/java/com/example/demo/domain/GolfCourse.java
+++ demo/src/main/java/com/example/demo/domain/GolfCourse.java
@@ -7,14 +7,28 @@ public class GolfCourse {
     public String name;
     public int courseID;
     public String paymentInfo;
-    public ArrayList teeSheet<TeeTime>;
+    public ArrayList <TeeTime> teeSheet;
 
-    public GolfCourse (String name, int courseID, String paymentInfo, ArrayList <TeeTime>){
+    public GolfCourse (String name, int courseID, String paymentInfo, ArrayList <TeeTime> teeSheet){
 
         this.name = name;
         this.courseID = courseID;
         this.paymentInfo = paymentInfo;
-        this.
+        this.teeSheet = teeSheet;
 
     }
+
+    public int getCourseID(){
+        return this.courseID;
+    }
+    public String getName(){
+        return  this.name;
+    }
+    public String getPaymentInfo(){
+        return this.paymentInfo;
+    }
+    public ArrayList <TeeTime> getTeeSheet(){
+        return this.teeSheet;
+    }
+
 }
