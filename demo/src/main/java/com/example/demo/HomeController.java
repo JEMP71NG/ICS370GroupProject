c1fc3d0585ca02477893d622f6db5387becb98d6
EMP71NG <nf3963kd@go.minnstate.edu>
2024-11-02 10:24:33 -0500

    Pushing "Unversioned files"

--- demo/src/main/java/com/example/demo/HomeController.java
+++ demo/src/main/java/com/example/demo/HomeController.java
@@ -20,19 +20,4 @@ import java.util.Map;
 
 public class HomeController {
 
-    @FXML
-    public void goToReserveTeeTime(ActionEvent event) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("first-view.fxml"));
-            Parent firstViewRoot = loader.load();
-            Scene firstViewScene = new Scene(firstViewRoot);
-
-            // Get the current stage and set the new scene
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setScene(firstViewScene);
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
