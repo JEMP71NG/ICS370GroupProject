6ed2a4229b7987e867d5889c07951b7b7a13965f
EMP71NG <nf3963kd@go.minnstate.edu>
2024-11-13 20:46:20 -0600

    First page is now the login page - Jacob

--- demo/src/main/java/com/example/demo/GolfApplication.java
+++ demo/src/main/java/com/example/demo/GolfApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class GolfApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(GolfApplication.class.getResource("home-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(GolfApplication.class.getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Golf Scheduler");
         stage.setScene(scene);
