28ab91924fc59578b6ba2c9dffa5dafbb7499147
EMP71NG <nf3963kd@go.minnstate.edu>
2024-11-13 20:10:59 -0600

    Created login-view.fxml and LoginController.java class. They don't really do anything yet, but the files exist now at least. - Jacob

---
+++ demo/src/main/java/com/example/demo/LoginController.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class LoginController {
+
+
+}
